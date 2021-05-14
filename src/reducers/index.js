3b7197a16420c401cdbac57c85258e8601f641ae
Ayushman09 <ayushmanchak@gmail.com>
2021-05-14 10:59:19 +0530

    Redux training

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import changeTheNumber from './UpDown';
+import  { combineReducers } from "redux";
+
+const rootReducer = combineReducers ({
+    changeTheNumber:changeTheNumber
+})
+export default rootReducer
